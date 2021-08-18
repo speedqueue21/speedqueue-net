e20e88342a6ae71d1947a405c2d3d2e85bdf7567
Weebkun <2002umpa@gmail.com>
2021-08-18 23:37:56 +0800

    added stall fields

--- src/main/java/com/weebkun/skipdq_net/Stall.java
+++ src/main/java/com/weebkun/skipdq_net/Stall.java
@@ -1,7 +1,12 @@
 package com.weebkun.skipdq_net;
 
+import com.squareup.moshi.Json;
+
 import java.io.Serializable;
 
 public class Stall extends Item implements Serializable {
-
+    public String email;
+    public String phone;
+    @Json(name = "food_court_id")
+    public String foodCourtId;
 }

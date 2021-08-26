65547b08aa1140371e5ca17c429e5760b4fb5217
Weebkun <2002umpa@gmail.com>
2021-08-26 23:21:34 +0800

    stuff

--- src/main/java/com/weebkun/skipdq_net/OrderItem.java
+++ src/main/java/com/weebkun/skipdq_net/OrderItem.java
@@ -1,10 +1,28 @@
 package com.weebkun.skipdq_net;
 
+import com.squareup.moshi.Json;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
 public class OrderItem {
     public String id;
+    @Json(name = "item_id")
     public String itemId;
+    @Json(name = "order_id")
     public String orderId;
+    public String name;
     public String selections;
     public int quantity;
     public double amount;
+
+    public Selections parseSelections() throws IOException {
+        // get selections
+        return new Moshi.Builder().build().adapter(Selections.class).fromJson(selections);
+    }
+
+    public static class Selections {
+        public double price;
+        public String[] selections;
+    }
 }
